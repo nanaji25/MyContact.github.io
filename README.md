5848a7dd6053d97fec3d5438341097ea0137c36b
nanaji25 <108604670+nanaji25@users.noreply.github.com>
2023-07-11 17:31:23 +0530

    My Contact

---
+++ README.md
@@ -0,0 +1,8 @@
+# Contact_Form
+Contact Form - To get a clear understanding of your Users, What exactly they want from You !!
+
+### Website Landing Page (Click On It)
+* [Web-Page](https://github.com/nanaji25/MyContact.github.io.git/)
+
+### The Way The Web Page Look On Large Screen 
+![Web_Page_Image](Contact_Form.png)
---
+++ script.js
@@ -0,0 +1,50 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+function validate(){
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var error_message = document.getElementById("error_message");
+  
+  error_message.style.padding = "10px";
+  
+  var text;
+  if(name.length < 5){
+    text = "Please Enter Valid Name";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(isNaN(phone) || phone.length != 10){
+    text = "Please Enter Valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(email.indexOf("@") == -1 || email.length < 6){
+    text = "Please Enter Valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(message.length <= 10){
+    text = "Please Enter More Than 10 Characters";
+    error_message.innerHTML = text;
+    return false;
+  }
+}
